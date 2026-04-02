a2a27beaec598b5de113262d3a0e21d3fafdea00
Navneet Tiwari <navneetgkp2011@gmail.com>
2026-04-02 17:21:49 +0530

    fix config env issue

--- app/config.py
+++ app/config.py
@@ -8,13 +8,17 @@ class Settings(BaseSettings):
     learner_table: str = "learner_details"
     bucket_name: str = "pragatididi-2025"
 
-    pg_host = os.getenv("POSTGRES_HOST")
-    pg_port = os.getenv("POSTGRES_PORT", "5432")
-    pg_user = os.getenv("POSTGRES_USER")
-    pg_password = os.getenv("POSTGRES_PASSWORD")
-    pg_database = os.getenv("POSTGRES_DB")
+    # ✅ Let pydantic read env variables automatically
+    pg_host: str
+    pg_port: int = 5432
+    pg_user: str
+    pg_password: str
+    pg_database: str
 
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
 
 
 @lru_cache
