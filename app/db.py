e5e4b4f898524d303711a7372c4c8eeff46a25ca
Navneet Tiwari <navneetgkp2011@gmail.com>
2026-04-02 16:56:39 +0530

    fix: add changes in db with variable

--- app/db.py
+++ app/db.py
@@ -1,12 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://impactnew:impact123@3.6.252.92:5432/impactdb"
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASS = os.getenv("POSTGRES_PASSWORD")
+DB_NAME = os.getenv("POSTGRES_DATABASE")
+DB_PORT = "5432"
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+print("USING DB:", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
